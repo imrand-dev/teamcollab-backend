30b2fad235026af8100f46cee595023fd0a36df3
Imran <imranrdev@gmail.com>
2024-06-25 15:42:09 +0600

    removed base_model

--- projectile/accounts/models.py
+++ projectile/accounts/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
 
@@ -5,20 +7,24 @@ from autoslug import AutoSlugField
 
 from accounts.managers import CustomUserManager
 
-from common.base_model import BaseModelWithUID
-
 
 def full_name(instance):
     if instance.first_name and instance.last_name:
         return f"{instance.first_name} {instance.last_name}".strip()
 
 
-class User(AbstractBaseUser, PermissionsMixin, BaseModelWithUID):
+class User(AbstractBaseUser, PermissionsMixin, models.Model):
     user_name = models.CharField(max_length=50, unique=True)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     slug = AutoSlugField(populate_from=full_name, editable=False, unique=True)
     email = models.EmailField(unique=True, db_index=True)
+
+    uid = models.UUIDField(
+        db_index=True, unique=True, default=uuid.uuid4, editable=False
+    )
+    date_joined = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
@@ -32,3 +38,6 @@ class User(AbstractBaseUser, PermissionsMixin, BaseModelWithUID):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        ordering = ["-date_joined"]
